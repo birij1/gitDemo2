b2e0fd47aca48a9196f8dad66156d316dd4b969c
birij1 <birij1@morgan.edu>
2025-04-29 17:05:34 -0400

    second commit

---
+++ src/gitDemos/helloAgain.java
@@ -0,0 +1,28 @@
+package gitDemos;
+
+import java.util.Scanner;
+
+public class helloAgain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Lets reverse the given String");
+		
+		System.out.println("Enter any String");
+		
+		Scanner sc= new Scanner (System.in);
+		
+		String A= sc.next();
+		
+		String rev= "";
+		
+		for (int i=A.length()-1; i>=0;i--) {
+			
+			rev+= A.charAt(i);
+			
+		}
+		
+		System.out.println(rev);
+	}
+
+}
